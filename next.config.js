9924a8601b7a51d72e9d5799d9bf0217523dee00
mwinscot <mwinscot@gmail.com>
2024-12-18 17:21:59 -0800

    Install dependencies and update config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
-  }
 };
 
 module.exports = nextConfig;
